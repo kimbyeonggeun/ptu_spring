f7b1db095acbd57a7e66cc290895f4539f910ad9
Byeong Geun Kim <84862538+kimbyeonggeun@users.noreply.github.com>
2023-09-26 17:06:48 +0900

    2nd week

---
+++ springMVC/project/ch03_pjt_03/src/main/java/ch03_pjt_03/MyCalculator.java
@@ -0,0 +1,9 @@
+package ch03_pjt_03;
+
+public class MyCalculator {
+	public void calculate(int fNum, int sNum, ICalculator calculator) {
+//		ICalculator 객체 주입
+		int value = calculator.doOperation(fNum, sNum);	//연산실행
+		System.out.println("result : "+value);
+	}
+}
